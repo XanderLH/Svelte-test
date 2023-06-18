68738eef13bfe35dc2df54629df3b85ce6c25bd3
XanderLH <134269111+XanderLH@users.noreply.github.com>
2023-06-18 13:31:10 +0200

    Update

--- src/routes/donate/todoStore.js
+++ src/routes/donate/todoStore.js
@@ -13,3 +13,19 @@ export const addTodo = (text) => {
 export const deleteTodo = (id) => {
     todos.update((todos) => todos.filter((todo)=> todo.id !== id))
 }
+
+export const toggleTodoCompleted = (id) => {
+    todos.update((todos) => {
+        let index = -1;
+        for (let i=0; i < todos.length; i++) {
+            if (todos[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        if (index !== -1) {
+            todos[index].completed = !todos[index].completed;
+        }
+        return todos;
+    })
+}
