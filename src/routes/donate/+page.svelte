8ed2864e4a174f71c6b2969c4acd94e1cd661909
XanderLH <134269111+XanderLH@users.noreply.github.com>
2023-06-18 14:44:30 +0200

    update

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,55 +1,26 @@
 <script>
-
-    import { TextInput, TextArea, Button, FormGroup, Form } from "carbon-components-svelte";
-        import { createForm } from "svelte-forms-lib";
-
-    const { form, errors, handleChange, handleSubmit, isSubmitting } = createForm({
-        initialValues: { name: "", email: "", message: "" },
-        onSubmit: async values => {
-            console.log(result);
-      }
-    });
-
-</script>
-
-<div class="page">
-
-    <h2>Contact Us</h2>
-    <p>Enter the details to get in touch with us. You can remove your email at any time after this.</p> <br/><br/>
-
-    <Form on:submit={handleSubmit}>
-
-        <FormGroup>
-            <TextInput labelText="Name" name="name" 
-                on:change={handleChange} bind:value={$form.name}/>
-        </FormGroup>
-
-        <FormGroup>
-            <TextInput labelText="Email" name="email" type="email" 
-            on:change={handleChange} bind:value={$form.email}/>
-        </FormGroup>
-
-
-        <FormGroup>
-            <TextArea labelText="Message" name="message" type="textarea"
-            on:change={handleChange} bind:value={$form.message}/>
-        </FormGroup>
-
-        <Button type="submit" disabled={$isSubmitting}>Submit</Button>
-    </Form>
-</div>
-
-<script>
     import { supabase } from "../../supabaseClient";
 
-        let apiResult = null;
+    import { createForm } from "svelte-forms-lib";
+    import * as yup from "yup";
+
+    import { TextInput, TextArea, Button, FormGroup, Form, InlineNotification } from "carbon-components-svelte";
+
+    const validationSchema = yup.object().shape({
+        name: yup.string().required('Please enter a name'),
+        email: yup.string().email().required('Please enter a valid email'),
+        message: yup.string().required('Please enter a message')
+    });
+
+    let apiResult = null;
 
     const { form, errors, handleChange, handleSubmit, isSubmitting, handleReset } = createForm({
         initialValues: { name: "", email: "", message: "" },
+        validationSchema: validationSchema,
         onSubmit: async values => {
 
             try {
-                var result = await supabase.from("contact").insert(values);
+                var result = await supabase.from("Contact").insert(values);
 
                 if (result.data != null) {
                     apiResult = true;
@@ -64,4 +35,59 @@
             handleReset();
       }
     });
+
 </script>
+
+<div class="page">
+
+    <h2>Contact Us</h2>
+    <p>Enter the details to get in touch with us. You can remove your email at any time after this.</p> <br/><br/>
+
+    {#if apiResult != null}
+
+        {#if apiResult == true}
+        <InlineNotification
+            lowContrast
+            kind="success"
+            title="Success:"
+            subtitle="Your message has been received"
+        />
+        {:else}
+        <InlineNotification lowContrast kind="error"
+            title="Error:"
+            subtitle="An internal server error occurred."
+        />
+
+        {/if}    
+    {/if}
+    <Form on:submit={handleSubmit}>
+
+        <FormGroup>
+            <TextInput labelText="Name" name="name" 
+                on:change={handleChange} bind:value={$form.name}
+                invalid={$errors.name.length > 0} invalidText={$errors.name}/>
+        </FormGroup>
+
+        <FormGroup>
+            <TextInput labelText="Email" name="email" type="email" 
+                on:change={handleChange} bind:value={$form.email}
+                invalid={$errors.email.length > 0} invalidText={$errors.email}/>
+        </FormGroup>
+
+
+        <FormGroup>
+            <TextArea labelText="Message" name="message" type="textarea"
+                on:change={handleChange} bind:value={$form.message}
+                invalid={$errors.message.length > 0} invalidText={$errors.message}/>
+        </FormGroup>
+
+
+        <Button type="submit" disabled={$isSubmitting}>Submit</Button>
+    </Form>
+</div>
+
+<style>
+    .page {
+        margin: 40px;
+    }
+</style>
