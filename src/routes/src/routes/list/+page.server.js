4a238e79b05be6fb3730e0227e9a445cbb283730
XanderLH <134269111+XanderLH@users.noreply.github.com>
2023-06-18 15:49:49 +0200

    update

--- src/routes/src/routes/list/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "../../../supabase";
-
-  export async function load() {
-    const { data } = await supabase.from("games").select();
-    return {
-      games: data ?? [],
-    };
-  }
