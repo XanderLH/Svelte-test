398a1f41a31cef69d33cebfbaf3899255420bff5
XanderLH <134269111+XanderLH@users.noreply.github.com>
2023-06-18 14:45:21 +0200

    update

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { supabase } from "../../supabaseClient";
+    import { supabase } from "../supabase";
 
     import { createForm } from "svelte-forms-lib";
     import * as yup from "yup";
