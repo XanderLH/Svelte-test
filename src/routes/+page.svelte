e8ce36215357afeaeb6f310a5fe806f48ddd8d45
XanderLH <134269111+XanderLH@users.noreply.github.com>
2023-06-19 02:39:01 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,8 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
+
+
 <section>
 	<h1>
 		<span class="Booo">
--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,5 +1,15 @@
 <script>
 	import pnf from '$lib/images/PNF.png';
+    let name
+    let email
+    let donations = 568
+    let money = 48950
+    function UpdateDonations() {
+        donations += 1
+        let jimmy = document.getElementById("don").value
+        money += Number(jimmy)
+    }
+
 </script>
 
 
@@ -16,6 +26,15 @@
       <br>
       <form id='formulaire'>
         <h1>L'argent n'a pas de valeur réelle, alors donnez-le nous !</h1>
+        <div class="container1">
+        <h6>{donations}</h6>
+        <h7>donations have already been made</h7>
+    </div>
+    <div class="container2">
+        <h8>{money}€</h8>
+        <h9>have been donated</h9>
+    </div>
+
         <br>
         <br>
 				<section class="name">
@@ -50,10 +69,12 @@
           <br>
         <br>
         <section class='submission'>
-        <input type="submit" id="bt" value="Save data to file" onclick="saveFile()">
+        <input type="submit" id="bt" value="Save data to file" on:click={UpdateDonations}>
+        
     </section>
   
-
+    <br>
+    <br>
 
       </form>
     </section>
@@ -62,13 +83,74 @@
 
 <style>
 
+
     #logo{
 	width: 400px;
 	position: relative;
     display: block;
   margin-left: auto;
   margin-right: auto;
-  top: 200px
+  top: 80px
 }
+
+.container1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position:absolute;
+  right: 500px;
+  top: 350px;
+  width: 200px;
+}
+
+.container2 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position:absolute;
+  right: -250px;
+  top: 350px;
+}
+
+
+h6 {
+    position: relative;
+    top: -70px;
+    color: rgb(21, 5, 36);
+    font-size: 3rem;
+	font-family: Fantasy;
+	border-radius: 10px;
+}
+
+h7 {
+    font-size: large;
+    font:700;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    color: rgb(21, 5, 36);
+}
+
+h8 {
+    position: relative;
+    top: -70px;
+    color: rgb(241, 225, 255);
+    font-size: 3rem;
+	font-family: Fantasy;
+	border-radius: 10px;
+
+}
+
+
+h9 {
+    font-size: large;
+    font:700;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    color: rgb(241, 225, 255);
+    position: relative;
+    top: -50px
+}
+
+
 
 </style>
